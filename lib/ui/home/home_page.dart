2750eb3c694c495b43cb5f07cdba38c7bff78daf
UooUzzz <uuumonazzz@gmail.com>
2024-12-06 12:29:11 +0900

    feat: TextField Done

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,11 +1,59 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController textEditingController = TextEditingController();
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  // 검색 함수(엔터시 바로 검색)
+  void onSearch(String text){
+    print('onSearch 호출됨');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Text('HomePage'),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: TextField(
+            maxLines: 1,
+            onSubmitted: onSearch,
+            controller: textEditingController,
+            decoration: InputDecoration(
+              hintText: '장소, 주소 검색',
+              hintStyle: TextStyle(
+                color: Colors.grey,
+              ),
+              border: MaterialStateOutlineInputBorder.resolveWith(
+                (states) {
+                  if (states.contains(WidgetState.focused)) {
+                    return OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(color: Colors.black),
+                    );
+                  }
+                  return OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.grey),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        body: Text('HomePage'),
+      ),
     );
   }
 }
---
+++ lib/ui/home/widgets/home_list_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Container(
+          height: 120,
+          width: double.infinity,
+          color: Colors.amber,
+        )
+      ],
+    );
+  }
+}
