22518aa32ea7c65f83da5350d5e175470dc5c866
UooUzzz <uuumonazzz@gmail.com>
2024-12-10 15:30:37 +0900

    feat: modify view model

--- lib/ui/home/search_view_model.dart
+++ lib/ui/home/search_view_model.dart
@@ -1,33 +1,38 @@
 import 'package:flutter_region_search_app/data/model/location.dart';
 import 'package:flutter_region_search_app/data/repository/location_repository.dart';
+import 'package:flutter_region_search_app/data/repository/vworld_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // 1. 상태 클래스 만들기
-// List<String>
+class HomeState {
+  List<Location> locations;
+  HomeState({required this.locations});
+}
 
 // 2. 뷰모델 만들기 - Notifier 상속
-class SearchViewModel extends AutoDisposeNotifier<List<Location>> {
+class SearchViewModel extends AutoDisposeNotifier<HomeState> {
   @override
-  List<Location> build() {
-    return [];
+  HomeState build() {
+    return HomeState(locations: []);
   }
 
   final locationRepository = LocationRepository();
+  final vworldRepository = VworldRepository();
 
-  Future<void> searchByName(String query) async {
+  void searchByName(String query) async {
     final result = await locationRepository.findByName(query);
-    print(result);
-    state = result;
+    state = HomeState(locations: result);
   }
-  Future<void> searchByCurrent(double lat, double lng) async {
-    final result = await locationRepository.findByCurrent(lat, lng);
-    print(result);
-    state = result;
+  void searchByCurrent(double lat, double lng) async {
+    final locationResult = await vworldRepository.findByCurrent(lat, lng);
+    if (locationResult.isNotEmpty) {
+      searchByName(locationResult.first);
+    }
   }
 }
 
 // 3. 뷰모델 관리자 만들기 - NotifierProvider 객체
 final searchViewModelProvider =
-    NotifierProvider.autoDispose<SearchViewModel, List<Location>>(() {
+    NotifierProvider.autoDispose<SearchViewModel, HomeState>(() {
   return SearchViewModel();
 });
